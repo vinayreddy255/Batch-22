d0b0b3bf02bc4ae4b96058f28a644c0ed5b5ed6f
vinayreddy255 <n.vinay255@gmail.com>
2026-06-16 08:59:25 +0530

    js

---
+++ HTML/06-16-2026/topics.js
@@ -0,0 +1,27 @@
+1995-> Brendan Eich
+interpreted
+casesentive
+
+-----------------
+data types
+functions
+contralStements
+operators
+
+events
+binding the data
+
+ES6-ES10
+-------------------
+data types
+
+primitev type
+  String
+  Number
+  Boolean
+  undefined
+  null
+NonPrimitive types
+  Object
+  Array
+  function
